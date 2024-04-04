3e624e148b23c04c211fe1fae21d4a7441f41773
Eloisa16 <ramossilvaeloisa226@gmail.com>
2024-04-04 08:18:50 -0300

    vetor

---
+++ ExCondicional/ExVetor2.java
@@ -0,0 +1,30 @@
+public class ExVetor2{
+    public static void main(String[] args){
+
+        /*
+        double [] notasAtividades = new double [5];
+        notasAtividades[0] = 6.0;
+        notasAtividades[1] = 8.0;
+        notasAtividades[2] = 4.0;
+        notasAtividades[3] = 2.0;
+        notasAtividades[4] = 9.0;
+        */
+        // Declaração com atribuição
+        
+        double [] notasAtividades = {6.0, 8.0, 4.0, 2.0, 9.0 };
+        double soma = 0;
+
+        // += -> atribuição com soma
+        // -= -> atribuição com subtração
+        // *= -> atribuição com multiplicação
+         // /= -> atribuição com divisão
+        for( int i = 0; i < 5; i++) {
+            System.out.println(notasAtividades[i]);
+           soma += notasAtividades[i]; // soma = soma + notasAtividades[i]
+       
+        }
+        System.out.println("Media das notas: " + soma /5 );
+          
+           }  
+         }
+    
