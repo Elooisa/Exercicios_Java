14c8e819e7698b9ae1ae4747d5e86d165300318d
Eloisa16 <ramossilvaeloisa226@gmail.com>
2024-04-05 09:12:54 -0300

    vetores

--- ExCondicional/ExVetor3.java
+++ ExCondicional/ExVetor3.java
@@ -1,19 +1,14 @@
 public class ExVetor3{
     public static void main(String[] args){
+ //Lógica dos múltiplos
+ //% -> mod: obtem o resto de uma divisão, muito utilizado para verificar a divisibilidade entre números
+ //Exemplo:
+ int x = 2;
+ int resto = x % 2;
+ if(resto == 0) {
+    System.out.println(x + " é par!!");
+ }
+   
 
-    int [] valores = new int [20];
-     Scanner teclado = new Scanner(System.in);
-       
-
-        for(int i = 0; i < valores.length; i++){
-       System.out.println("Digite o" + i + 1 + "º valor");
-       valores[i] = teclado.nextLine();
-
-    }
-    
-    for(int i = 0; i < valores.length; i++){
-        System.out.println(i + 1 + "º valor" + valores[i]);
-
-}
     }
 }
