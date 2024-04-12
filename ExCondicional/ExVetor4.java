a9e90756a413958ea3326f43518993600c952442
Eloisa16 <ramossilvaeloisa226@gmail.com>
2024-04-12 09:12:02 -0300

    vetor2

---
+++ ExCondicional/ExVetor4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ExVetor4{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] palavrasEscondidas {"java","arrays","vetor","variavel","objetos"};
+        String temaJogo = "Objetos de programação";
+
+        System.out.println("Tema: " + temaJogo);
+
+        int tentativas = 3;
+        boolean acertou = false;
+
+        while (tentativas > 0 &&! acertou) {
+            
+        }
+        
+       
+    }
+}
