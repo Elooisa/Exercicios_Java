39089597c7d3b065cbcb9109a5a3823214a700d0
Eloisa16 <ramossilvaeloisa226@gmail.com>
2024-04-29 12:17:36 -0300

    vetores

---
+++ ExVetor5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ExVetor5{
+    public static void main(String[]args){
+        
+        int numeros []  = new int[3];
+        Scanner teclado = new Scanner(System.in);
+        double soma = 0;
+
+        int i = 0;
+        while(i < numeros.length) {
+        System.out.println("Digite um número inteiro");
+        int n = teclado.nextInt();
+
+        if(n >= 0  && n <= 38){
+           numeros[i] = n;
+        }else{
+           i--;//decrementa um valor ao contador (i)
+        }
+         i++; //incrementa um valor ao contador (i)
+        }
+
+           for( i = 0; i < numeros.length; i++ ){
+            if(numeros[i] % 4 == 0){
+                soma += numeros[i];          
+            }
+           
+
+           }
+            
+        }
+    
+     }
+
+
